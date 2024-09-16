098fdbf39f3ebcb4da07392f36c64052b24ef0cc
Cathrine Vaage <cathrine.vaage@gmail.com>
2024-09-16 12:41:13 +0200

    add distinct exceptions for errors and bad requests

--- src/Client.php
+++ src/Client.php
@@ -1,13 +1,17 @@
 <?php
+
 namespace Apility\Visma;
 
+use Apility\Visma\Exceptions\VismaBadRequestException;
+use Apility\Visma\Exceptions\VismaErrorException;
+use Exception;
 use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Psr7\Request;
 use SimpleXMLElement;
 use GuzzleHttp\Client as Guzzle;
-use Exception;
 
-class Client {
-
+class Client
+{
     private string $guid;
     private Guzzle $client;
     private string $clientID;
@@ -18,82 +22,86 @@ class Client {
      * @param string $token
      * @param string $clientId
      */
-	function __construct ($url, string $token, string $clientId) {
+    function __construct($url, string $token, string $clientId)
+    {
+        $this->client = new Guzzle([
+            'base_uri' => $url
+        ]);
 
-		$this->client = new Guzzle([
-			'base_uri' => $url
-		]);
-
-		$this->guid = $token;
-		$this->clientID = $clientId;
-
-	}
+        $this->guid = $token;
+        $this->clientID = $clientId;
+    }
 
     /**
      * Submits data to Visma
-     * @param string $url
+     * @param string $uri
      * @param SimpleXMLElement $payload
-     * @return SimpleXMLElement Response
-     * @throws GuzzleException|Exception
+     * @return SimpleXMLElement
+     * @throws GuzzleException
+     * @throws VismaErrorException
+     * @throws VismaBadRequestException
+     * @throws Exception
      */
-    public function post (string $url, SimpleXMLElement $payload): SimpleXMLElement {
-
+    public function post(
+        string $uri,
+        SimpleXMLElement $payload
+    ): SimpleXMLElement {
         $header = $payload->addChild('Header');
-	    $header->addChild('ClientId', $this->clientID);
-	    $header->addChild('Guid', $this->guid);
+        $header->addChild('ClientId', $this->clientID);
+        $header->addChild('Guid', $this->guid);
 
-		$status = $payload->addChild('Status');
-	    $status->addChild('MessageId');
-	    $status->addChild('Message');
-	    $status->addChild('MessageDetail');
+        $status = $payload->addChild('Status');
+        $status->addChild('MessageId');
+        $status->addChild('Message');
+        $status->addChild('MessageDetail');
 
-	    $payload = (string) $payload->asXML();
+        $request = new Request('POST', $uri, [], $payload->asXML());
+        $response = $this->client->send($request);
 
-	    $request = $this->client->post($url, ['body' => $payload]);
-	    $response = (string) $request->getBody();
-	    $response = new SimpleXMLElement($response);
+        $responseXml = new SimpleXMLElement($response->getBody());
 
-	    if (isset($response->Errors)) {
-	      $errors = $response->Errors[0];
-	      $details = $errors->Details[0];
-	      $message = $errors->attributes->Status[0];
-	      $message .= ', ' . $details->attributes->Method[0];
-	      $message .= ': ' . $details->attributes->Message[0];
+        if (isset($responseXml->Errors)) {
+            throw new VismaErrorException(
+                $responseXml->Errors[0]->Details[0]->attributes->Message[0],
+                intval($responseXml->Errors[0]->attributes->Status[0]),
+                $responseXml->Errors[0]->Details[0]->attributes->Method[0],
+                $request,
+                $response
+            );
+        }
 
-	      throw new Exception($message);
-	    }
+        if (
+            isset($responseXml->Status)
+            && intval($responseXml->Status->MessageId[0]) !== 0
+        ) {
+            throw new VismaBadRequestException(
+                $responseXml->Status->Message[0],
+                intval($responseXml->Status->MessageId[0]),
+                $responseXml->Status->MessageDetail[0],
+                $request,
+                $response,
+            );
+        }
 
-	    if (isset($response->Status) && intval($response->Status->MessageId[0])) {
-	      $message = $response->Status->Message[0];
-	      if ($response->Status->MessageDetail[0]) {
-	        $message .= ', ' . $response->StatusMessageDetail[0];
-	      }
+        return $responseXml;
+    }
 
-		  throw new Exception($message);
+    /**
+     * @param SimpleXMLElement $payload
+     * @return string
+     */
+    public function debug(SimpleXMLElement $payload): string
+    {
+        $header = $payload->addChild('Header');
+        $header->addChild('ClientId', $this->clientID);
+        $header->addChild('Guid', $this->guid);
 
-	    }
+        $status = $payload->addChild('Status');
+        $status->addChild('MessageId');
+        $status->addChild('Message');
+        $status->addChild('MessageDetail');
 
-		return $response;
-
-	}
-
-  /**
-   * @param SimpleXMLElement $payload
-   * @return string
-   */
-	public function debug(SimpleXMLElement $payload): string {
-
-		$header = $payload->addChild('Header');
-	    $header->addChild('ClientId', $this->clientID);
-	    $header->addChild('Guid', $this->guid);
-
-		$status = $payload->addChild('Status');
-	    $status->addChild('MessageId');
-	    $status->addChild('Message');
-	    $status->addChild('MessageDetail');
-
-		$payload = (string) $payload->asXML();
-		return $payload;
-	}
-
+        $payload = (string) $payload->asXML();
+        return $payload;
+    }
 }
